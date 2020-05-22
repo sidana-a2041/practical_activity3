37cda625b405204aaeb79e4f3b6f1a9f3afbef5d
Aashish <sidanaa1@student.douglascollege.ca>
2020-05-22 13:54:17 -0700

    added src to track

---
+++ src/myjavaproject/myAppTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myjavaproject;
+
+/**
+ * @author aashi
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
